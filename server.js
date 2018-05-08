b20ab89c5fa9b8453dd804b98cbd747233aae687
John <johnngo@Johns-MacBook-Pro.local>
2018-05-08 08:47:13 -0700

    connect mongoose to database

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+//setup mongoose to connect to the database mongoDB
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/friendDB').then(mongo => {
+  console.log('connected to database');
+}).catch(err => {
+  console.log('Error connecting to database', err);
+});
+
 const server = express();
 
 server.use(helmet());
