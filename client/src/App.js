4564ccb9c90b227ce0a467bf71ce34d524e7c2c3
John <johnngo@Johns-MacBook-Pro.local>
2018-05-08 10:31:12 -0700

    added react app, display first names

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    friends: []
+  }
+  componentDidMount() {
+    this.getFriends()
+  }
+
+  getFriends = () => {
+    axios.get('http://localhost:5000/api/friends')
+    .then(res => {
+      this.setState({ friends: res.data })
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+      {this.state.friends.map(friend => 
+        <div>
+          <h1>{friend.firstName}</h1>
+        </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
